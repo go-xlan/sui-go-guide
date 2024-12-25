fe15704e73abff1ac0dcf59a1d7604ff03f30149
yangyile <gogitv5git@github.com>
2024-12-26 00:30:27 +0700

    简单添加样例

---
+++ go.mod
@@ -0,0 +1,24 @@
+module github.com/go-xlan/sui-go-guide
+
+go 1.22.8
+
+require (
+	github.com/go-resty/resty/v2 v2.16.2
+	github.com/yyle88/must v0.0.12
+	github.com/yyle88/neatjson v0.0.10
+	github.com/yyle88/zaplog v0.0.17
+	go.uber.org/zap v1.27.0
+)
+
+require (
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/yyle88/done v1.0.20 // indirect
+	github.com/yyle88/erero v1.0.15 // indirect
+	github.com/yyle88/mutexmap v1.0.9 // indirect
+	github.com/yyle88/sure v0.0.34 // indirect
+	github.com/yyle88/syntaxgo v0.0.43 // indirect
+	github.com/yyle88/tern v0.0.4 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/net v0.33.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
+)
---
+++ internal/demos/main_suix_getBalance/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/go-resty/resty/v2"
+	"github.com/go-xlan/sui-go-guide/suijsonrpc"
+	"github.com/yyle88/must"
+	"github.com/yyle88/neatjson/neatjsons"
+	"github.com/yyle88/zaplog"
+	"go.uber.org/zap"
+)
+
+type RespType struct {
+	Data []struct {
+		Balance             string `json:"balance"`
+		CoinObjectId        string `json:"coinObjectId"`
+		CoinType            string `json:"coinType"`
+		Digest              string `json:"digest"`
+		PreviousTransaction string `json:"previousTransaction"`
+		Version             string `json:"version"`
+	} `json:"data"`
+	HasNextPage bool   `json:"hasNextPage"`
+	NextCursor  string `json:"nextCursor"`
+}
+
+func main() {
+	address := "0x207ed5c0ad36b96c730ed0f71e3c26a0ffb59bc20ab21d08067ca4c035d4d062"
+
+	request := suijsonrpc.JsonRpcRequest{
+		Jsonrpc: "2.0",
+		Method:  "suix_getCoins",
+		Params: []any{
+			address,
+		},
+		ID: 1,
+	}
+
+	rpcResponse := &suijsonrpc.JsonRpcResponse[RespType]{}
+
+	response, err := resty.New().
+		SetTimeout(time.Minute).
+		R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(request).
+		SetResult(rpcResponse).
+		Post("https://fullnode.testnet.sui.io/")
+	must.Done(err)
+	must.Same(http.StatusOK, response.StatusCode())
+
+	zaplog.SUG.Debugln("Response Raw:", neatjsons.SxB(response.Body()))
+
+	must.Null(rpcResponse.Error)
+
+	zaplog.SUG.Debugln("Response Msg:", neatjsons.S(rpcResponse))
+
+	for _, coin := range rpcResponse.Result.Data {
+		zaplog.LOG.Debug("coin", zap.String("balance", coin.Balance), zap.String("coin_type", coin.CoinType))
+	}
+}
